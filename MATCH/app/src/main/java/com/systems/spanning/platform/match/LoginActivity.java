dd61a07d521a4f3fb5fc7a9bf32775b2d5da1aa3
Sandbom <simon.sandbom@gmail.com>
2018-02-05 13:38:25 +0100

    småsaker

--- MATCH/app/src/main/java/com/systems/spanning/platform/match/LoginActivity.java
+++ MATCH/app/src/main/java/com/systems/spanning/platform/match/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.systems.spanning.platform.match;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -17,45 +18,17 @@ import android.widget.CheckBox;
 public class LoginActivity extends AppCompatActivity {
 
 
-    private final AppCompatActivity activity = LoginActivity.this;
-
-    private NestedScrollView nestedScrollView;
-
-    private TextInputLayout textInputLayoutEmail;
-    private TextInputLayout textInputLayoutPassword;
-
-    private TextInputEditText textInputEditTextEmail;
-    private TextInputEditText textInputEditTextPassword;
-
-    private AppCompatButton appCompatButtonLogin;
-
-    private AppCompatTextView textViewLinkRegister;
-
-   // private InputValidation inputValidation;
-   // private DatabaseHelper databaseHelper;
-
-    private String password,email;
-    private CheckBox checkBox;
-    SharedPreferences loginPreferences; //= PreferenceManager.getDefaultSharedPreferences();
-    SharedPreferences.Editor loginPrefsEditor;
-    private Boolean saveLogin;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
-        });
+
+    public void Registerclick(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+
     }
 
 }
