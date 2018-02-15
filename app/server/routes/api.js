2aa0495d1b4d642fd4503c38a88726b299128ae0
matildatrodin <matilda@trodin.com>
2018-02-15 15:07:48 +0100

    updated

--- app/server/routes/api.js
+++ app/server/routes/api.js
@@ -1,18 +1,13 @@
 //dependencies:
 var express = require('express');
 var router = express.Router();
-var router1 = express.Router();
 
 //get models:
 var Users = require('../models/users');
-var Activities = require('../models/activities');
 
 //routes:
 Users.methods(['get', 'post', 'put', 'delete']);
 Users.register(router, '/users');
-Activities.methods(['get', 'post', 'put', 'delete']);
-Activities.register(router1, '/activities');
 
 //return router:
 module.exports = router;
-module.exports = router1;
