d8b8bb2575520c2eef24802b39441c9918d15d17
matildatrodin <matilda@trodin.com>
2018-02-19 14:24:32 +0100

    controllers för activities och users

---
+++ app/server/Controllers/activitiesController.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose'),
+    Activity = mongoose.model('Activities');
+
+exports.list_all_activities = function(req, res){
+    Task.find({}, function(err, activity){
+        if (err)
+            res.send(err);
+        res.json(activity);
+    });
+};
+
+exports.create_a_activity = function(req, res){
+    var new_activity = new Activity(req.body);
+    new_activity.save(function(err, activity){
+        if (err)
+            res.send(err)
+        res.json(activity);
+    });
+};
+
+exports.read_a_task = function(req, res){
+    Activity.findById(req.params.activityId, function(err, activity){
+        if (err)
+          res.send(err);
+        res.json(activity);
+    });
+};
+
+exports.update_a_activity = function(req, res){
+    Activity.findOneAndUpdate({_id: req.params.activityId}, req.body, {new: true}, function(err, activity){
+        if (err)
+            res.send(err);
+        res.json(activity);
+    });
+};
+
+exports.delete_a_task = function(req, res){
+
+    Activity.remove({
+        _id: req.params.activityId
+    }, function(err, activity) {
+        if (err)
+            res.send(err);
+        res.json({ message: 'Activity successfully deleted' });
+    });
+};
