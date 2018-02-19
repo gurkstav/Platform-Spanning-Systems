8bffc03cd6ee0e6bfae7dc2a8b655584b59294f5
matildatrodin <matilda@trodin.com>
2018-02-19 14:28:04 +0100

    updaterad för att matcha api

--- app/server/models/activities.js
+++ app/server/models/activities.js
@@ -1,19 +1,19 @@
-//dependencies:
-var restful = require('node-restful');
-var mongoose = restful.mongoose;
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 //Schema:
-var activitiesSchema = new mongoose.Schema({
-        type: {type: String, required: true},
-        date: {type: Number, required: true},
-        location: {type: String, required: true},
-        min_participants: {type: Number, required: true},
-        max_participants: {type: Number}
+var activitiesSchema = new Schema({
+    type: {type: String, required: true},
+    date: {type: Date, default: Date.now, required: true},
+    location: {type: String, required: true},
+    min_participants: {type: Number, required: true},
+    max_participants: {type: Number}
         //contact: {type: mongoose.Schema.Types.ObjectId, ref: "full_name"}
     }
 
 );
 
 //return models:
-module.exports = restful.model('activities', activitiesSchema);
+module.exports = mongoose.model('activities', activitiesSchema);
 
