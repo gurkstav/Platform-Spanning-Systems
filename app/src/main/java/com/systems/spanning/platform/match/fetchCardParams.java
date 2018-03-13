9226bbaed3a7fde366a6442e799283537ec64b0f
marcusfroling <colamackan@hotmail.com>
2018-03-13 15:25:57 +0100

    Changed Match to match database

--- app/src/main/java/com/systems/spanning/platform/match/fetchCardParams.java
+++ app/src/main/java/com/systems/spanning/platform/match/fetchCardParams.java
@@ -12,21 +12,24 @@ public class fetchCardParams {
     public static ArrayList<Match> fetchParams(JSONArray result) {
         ArrayList<Match> matchList = new ArrayList<>();
         if (result.length() == 0) {
-            Match a = new Match(null, null, null, null, null, null, 0);
+            Match a = new Match(null,null,null,null,null,null,null,null,null,0);
             matchList.add(a);
         }
         else {
             for (int i = 0; i < result.length(); i++) {
                 try {
                     JSONObject activity = result.getJSONObject(i);
+                    String title = activity.getString("title");
+                    String description = activity.getString("description");
                     String type = activity.getString("type");
                     String date = activity.getString("date");
+                    String time = activity.getString("time");
                     String location = activity.getString("location");
                     String max_part = activity.getString("max_participants");
                     String min_part = activity.getString("min_participants");
-                    String users_email = activity.getString("users_email");
+                    String email = activity.getString("email");
 
-                    Match a = new Match(type, date, location, min_part, max_part, users_email, 1);
+                    Match a = new Match(title,description, type, date, time, location, min_part, max_part, email, 1);
                     matchList.add(a);
                 } catch (JSONException e) {
                 }
